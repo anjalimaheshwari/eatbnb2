f57b6b0b3ab1ab1c97f28dde1745f7b7625157ba
anjalimaheshwari <anjalimaheshwari2@gmail.com>
2018-07-08 15:09:39 -0400

    new component

---
+++ src/app/eatbnb/eatbnb.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: './eatbnb.component.html'
+})
+export class EatbnbComponent {
+    public pageTitle: string = 'EatBnb';
+}
